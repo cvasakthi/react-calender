3dadf1675b7081e5b89a16c5ae30bcb5b3787a88
sivasakthi <cvasakthi96@gmail.com>
2024-10-07 13:25:52 +0530

    slideover component update

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 import CustomCalendar from "./component/calender/CustomCalendar";
 import { fetchAllEvents } from "./api/eventsList";
 import { EventI } from "./type/events";
+import SlideOver from "./component/SlideOver";
 
 function App() {
   const [eventsList, setEventsList] = useState<EventI[]>([]);
+  const [selected, setSelected] = useState<EventI[]>([]);
   const [loading, setLoading] = useState(true);
+  const [showSlideover, setShowSlideover] = useState<boolean>(false);
   useEffect(() => {
     fetchAllEvents()
       .then((response) => {
@@ -19,21 +22,36 @@ function App() {
       });
   }, []);
   return (
-    <div className="h-screen w-screen">
-      <header className="w-full bg-gray-50 p-4 flex sticky top-0 z-[5] shadow">
-        <h4 className="mx-auto text-2xl font-semibold text-purple-800">
-          Calender
-        </h4>
-      </header>
-      <div className="mx-auto container mt-2">
-        <CustomCalendar
-          data={eventsList}
-          onEventClick={(data) => {
-            console.log(data);
+    <>
+      {showSlideover && (
+        <SlideOver
+          onClose={() => {
+            setShowSlideover(false);
+            setSelected([]);
           }}
-        />
+          open={showSlideover}
+          title="Meetings"
+        >
+          <div>Slide over component</div>
+        </SlideOver>
+      )}
+      <div className="h-screen w-screen">
+        <header className="w-full bg-gray-50 p-4 flex sticky top-0 z-[5] shadow">
+          <h4 className="mx-auto text-2xl font-semibold text-purple-800">
+            Calender
+          </h4>
+        </header>
+        <div className="mx-auto container my-10">
+          <CustomCalendar
+            data={eventsList}
+            onEventClick={(data) => {
+              setSelected(data);
+              setShowSlideover((prev) => !prev);
+            }}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/component/SlideOver.tsx
@@ -0,0 +1,97 @@
+import { FC, ReactNode, useEffect, useRef, useState } from "react";
+
+interface SlideOverProps {
+  title: string;
+  open: boolean;
+  onClose: () => void;
+  children: ReactNode;
+  outsideClickClose?: boolean;
+}
+
+const SlideOver: FC<SlideOverProps> = ({
+  title,
+  onClose,
+  children,
+  open,
+  outsideClickClose = true,
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(open);
+  const slideOverRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    setIsOpen(open);
+  }, [open]);
+
+  const toggleSlideOver = () => {
+    setIsOpen(!isOpen);
+    if (isOpen) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        slideOverRef.current &&
+        !slideOverRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+        onClose();
+      }
+    };
+
+    if (isOpen && outsideClickClose) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen, onClose, outsideClickClose]);
+
+  return (
+    <div>
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-40"
+          onClick={toggleSlideOver}
+        />
+      )}
+      <div
+        ref={slideOverRef}
+        className={`fixed inset-y-0 right-0 z-50 w-[400px] bg-white shadow-lg transform transition-transform${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="p-6">
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-xl font-semibold">{title}</h2>
+            <button
+              onClick={toggleSlideOver}
+              className="bg-blue-600 text-white rounded-full hover:bg-blue-700 transition cursor-pointer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SlideOver;
