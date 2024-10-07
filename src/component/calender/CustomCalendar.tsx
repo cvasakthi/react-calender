5fbe4511cd29f3ce2a86616564f1b6d9c7ff0c55
sivasakthi <cvasakthi96@gmail.com>
2024-10-07 07:06:41 +0530

    wip

--- src/component/calender/CustomCalendar.tsx
+++ src/component/calender/CustomCalendar.tsx
@@ -1,24 +1,46 @@
-import React, { useState } from "react";
-import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import "./CustomCalendar.css"; // Custom styles
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { useEffect, useState } from "react";
 import { EventI } from "../../type/events";
+import "./CustomCalendar.css"; // Custom styles
 
 interface CalenderProps {
   data: EventI[];
 }
 const CustomCalendar = ({ data }: CalenderProps) => {
-  const [events] = useState([
-    { title: "Event 1", date: "2024-10-08" },
-    { title: "Event 2", date: "2024-10-09" },
-  ]);
+  const [eventsList, setEventsList] = useState<any[]>([]);
+  useEffect(() => {
+    const mappedEvents = data.map((event) => ({
+      id: String(event.id),
+      title: event.desc,
+      allDay: false,
+      start: new Date(event.start),
+      end: new Date(event.end),
+      extendedProps: { ...event },
+    }));
+    setEventsList(mappedEvents);
+  }, [data]);
+
+  console.log({ eventsList });
+
+  // const handleEventClick = (evt: any) => {
+  //   console.log({ evt });
+  // };
+  const renderEventContent = (eventInfo: any) => {
+    return (
+      <div className="bg-white min-w-fit min-h-40">
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </div>
+    );
+  };
   return (
     <FullCalendar
       plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
       initialView="timeGridWeek"
-      events={events}
+      events={eventsList}
       themeSystem="standard"
       nowIndicator={true}
       selectable={false}
@@ -28,6 +50,8 @@ const CustomCalendar = ({ data }: CalenderProps) => {
         center: "title",
         right: "dayGridMonth,timeGridWeek,timeGridDay",
       }}
+      // eventClick={handleEventClick}
+      // eventContent={renderEventContent}
     />
   );
 };
